82c5ea8e259e9b09d51485e6fcd6a3b90f31dfc6
ianpaul0520 <ianpaul0520@gmail.com>
2020-07-01 20:34:12 +0800

    First Commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let specialUnicode = [
+  {
+    name: "Unicode Right",
+    code: "&#x276F",
+  },
+  {
+    name: "Comet",ls
+    
+  },
+];
+
+let button = document.getElementById("create-note-button");
+button.addEventListener("click", function () {
+  
+});
