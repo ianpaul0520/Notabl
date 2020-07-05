75c0d9ab5a5d7723bc376ea6aa93c67277ef9d15
ianpaul0520 <ianpaul0520@gmail.com>
2020-07-05 10:27:40 +0800

    Branching out to ModalEditor and created functional modal editor

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-let mySelect = document.getElementById("mySelect");
-
-mySelect.onchange = function () {
-  console.log(Math.floor(Math.random()) * Math.floor(100));
+let classesObject = {
+  isActive: "is-active",
 };
-console.log(mySelect.options);
+
+let activateButton = document.getElementById("activate-button");
+let myModal = document.getElementById("my-modal");
+let modalBackground = document.getElementById("modal-background");
+let modalClose = document.getElementById("modal-close");
+
+activateButton.addEventListener("click", function () {
+  myModal.classList.add(classesObject.isActive);
+});
+
+modalBackground.addEventListener("click", function () {
+  myModal.classList.remove(classesObject.isActive);
+});
+
+function addClassFn(classname, element) {
+  element.classList.add(classname);
+}
+
+function removeClassFn(classname, element) {
+  element.classList.remove(classname);
+}
